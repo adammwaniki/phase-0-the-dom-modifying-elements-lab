be90c98746a0372ab337fac8ddca781e3a488e1b
Adam Mwaniki <adamndegwa@protonmail.com>
2024-03-30 18:14:44 +0300

    Completes the dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Write your code here
+//removing the element main
+document.getElementById("main").remove();
+
+//creating a newHeader variable that points to the h1 node
+const newHeader  = document.createElement("h1");
+
+//giving this new element an id of 'victory'
+newHeader.setAttribute('id',"victory");
+
+//setting the text of this new element to "Adam-Mwaniki is the champion"
+newHeader.textContent ="Adam Mwaniki is the champion";
